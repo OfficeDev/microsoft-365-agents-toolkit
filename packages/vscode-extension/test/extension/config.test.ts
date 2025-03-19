e49644675b0fa037ed3225737f9fcfa364e7566a
Bowen Song <bowsong@microsoft.com>
2025-03-19 08:55:54 +0800

    perf(kiota): enable kiota if kiota extension installed

--- packages/vscode-extension/test/extension/config.test.ts
+++ packages/vscode-extension/test/extension/config.test.ts
@@ -1,10 +1,11 @@
-import { LogLevel } from "@microsoft/teamsfx-api";
+import { LogLevel, ok } from "@microsoft/teamsfx-api";
 import * as chai from "chai";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
 import VsCodeLogInstance from "../../src/commonlib/log";
 import { configMgr } from "../../src/config";
 import { ExtTelemetry } from "../../src/telemetry/extTelemetry";
+import * as vsc_ui from "../../src/qm/vsc_ui";
 
 describe("configMgr", () => {
   const sanbox = sinon.createSandbox();
@@ -93,6 +94,98 @@ describe("configMgr", () => {
       const stub = sanbox.stub(configMgr, "loadConfigs").returns();
       configMgr.registerConfigChangeCallback();
       chai.assert.isTrue(stub.called);
+    });
+  });
+
+  describe("checkKiotaInstallation", async () => {
+    afterEach(() => {
+      sanbox.restore();
+    });
+    beforeEach(() => {
+      sanbox.stub(vsc_ui, "VS_CODE_UI").value(new vsc_ui.VsCodeUI(<vscode.ExtensionContext>{}));
+    });
+    it("should skip if current value is enabled", async () => {
+      const configStub = sanbox.stub(vscode.workspace, "getConfiguration").returns({
+        get: () => {
+          return "Enabled";
+        },
+        update: () => {
+          return;
+        },
+      } as any);
+      await configMgr.checkKiotaInstallation();
+      chai.assert.isTrue(configStub.calledOnce);
+    });
+    it("should set enabled if previous value is true", async () => {
+      sanbox.stub(vscode.workspace, "getConfiguration").returns({
+        get: (key: string) => {
+          if (key === "enableMicrosoftKiota") {
+            return true;
+          } else {
+            return "Undefined";
+          }
+        },
+        update: (key: string, value: string) => {
+          chai.assert.equal(key, "enableMicrosoftKiotaString");
+          chai.assert.equal(value, "Enabled");
+          return;
+        },
+      } as any);
+      await configMgr.checkKiotaInstallation();
+    });
+    it("should ask user and set enabled", async () => {
+      sanbox.stub(vscode.workspace, "getConfiguration").returns({
+        get: (key: string) => {
+          if (key === "enableMicrosoftKiota") {
+            return undefined;
+          } else {
+            return "Undefined";
+          }
+        },
+        update: (key: string, value: string) => {
+          chai.assert.equal(key, "enableMicrosoftKiotaString");
+          chai.assert.equal(value, "Enabled");
+          return;
+        },
+      } as any);
+      sanbox.stub(vsc_ui.VS_CODE_UI, "showMessage").resolves(ok("Yes"));
+      await configMgr.checkKiotaInstallation();
+    });
+    it("should ask user and set disabled", async () => {
+      sanbox.stub(vscode.workspace, "getConfiguration").returns({
+        get: (key: string) => {
+          if (key === "enableMicrosoftKiota") {
+            return undefined;
+          } else {
+            return "Undefined";
+          }
+        },
+        update: (key: string, value: string) => {
+          chai.assert.equal(key, "enableMicrosoftKiotaString");
+          chai.assert.equal(value, "Disabled");
+          return;
+        },
+      } as any);
+      sanbox.stub(vsc_ui.VS_CODE_UI, "showMessage").resolves(ok("No"));
+      await configMgr.checkKiotaInstallation();
+    });
+    it("should ask user and set disabled if user cancel", async () => {
+      sanbox.stub(vscode.workspace, "getConfiguration").returns({
+        get: (key: string) => {
+          if (key === "enableMicrosoftKiota") {
+            return undefined;
+          } else {
+            return "Undefined";
+          }
+        },
+        update: (key: string, value: string) => {
+          chai.assert.equal(key, "enableMicrosoftKiotaString");
+          chai.assert.equal(value, "Disabled");
+          return;
+        },
+      } as any);
+      sanbox.stub(vsc_ui.VS_CODE_UI, "showMessage").rejects(new Error("error"));
+      await configMgr.checkKiotaInstallation();
     });
   });
 });
