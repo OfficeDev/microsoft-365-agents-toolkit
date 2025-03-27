8e49a0e581eaec78923b645c6af7da87122bf68a
huajiezhang <huajiezhang@microsoft.com>
2025-03-27 15:15:13 +0800

    feat: add warning message if the old cli command is used

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -11,6 +11,7 @@ import "./console/screen";
 import * as constants from "./constants";
 import cliTelemetry from "./telemetry/cliTelemetry";
 import { TelemetryProperty } from "./telemetry/cliTelemetryEvents";
+import { logger } from "./commonlib/logger";
 
 export function initTelemetryReporter(): void {
   const cliPackage = JSON.parse(fs.readFileSync(path.join(__dirname, "/../package.json"), "utf8"));
@@ -28,6 +29,11 @@ export function initTelemetryReporter(): void {
 export async function start(): Promise<void> {
   initTelemetryReporter();
   const binName = process.env.TEAMSFX_CLI_BIN_NAME as string;
+  if (binName === "teamsapp") {
+    logger.warning(
+      `Deprecation Warning: The CLI package "@microsoft/teamsapp-cli" and its "teamsapp" command are renamed to "@microsoft/m365agentstoolkit-cli" and "m365agents" respectively. The old command "teamsapp" will be retired soon. Please switch to the new command "m365agents" and update your workflows accordingly.`
+    );
+  }
   cliTelemetry.reporter?.addSharedProperty(TelemetryProperty.BinName, binName); // trigger binary name for telemetry
   return startNewUX(binName);
 }
