631e31327da8bf95a85a1d37b929d0ad218ffa50
frankqianms <frankqian@microsoft.com>
2025-04-01 17:11:35 +0800

    feat: vsc ui for weather agent

--- packages/fx-core/src/question/constants.ts
+++ packages/fx-core/src/question/constants.ts
@@ -533,7 +533,7 @@ export class CapabilityOptions {
     const capabilities = [
       CapabilityOptions.empty(),
       ...CapabilityOptions.agents(),
-      ...CapabilityOptions.customCopilots(),
+      ...CapabilityOptions.customCopilots(inputs),
       ...CapabilityOptions.bots(inputs),
       CapabilityOptions.nonSsoTab(),
       CapabilityOptions.tab(),
@@ -589,12 +589,20 @@ export class CapabilityOptions {
     return [CapabilityOptions.declarativeAgent()];
   }
 
-  static customCopilots(): OptionItem[] {
-    return [
-      CapabilityOptions.customCopilotBasic(),
-      CapabilityOptions.customCopilotRag(),
-      CapabilityOptions.customCopilotAssistant(),
-    ];
+  static customCopilots(inputs?: Inputs): OptionItem[] {
+    return inputs !== undefined && getRuntime(inputs) === RuntimeOptions.DotNet().id
+      ? [
+          CapabilityOptions.customCopilotBasic(),
+          CapabilityOptions.customCopilotRag(),
+          CapabilityOptions.customCopilotAssistant(),
+          // customCopilotWeather is currently only for dotnet
+          CapabilityOptions.customCopilotWeather(),
+        ]
+      : [
+          CapabilityOptions.customCopilotBasic(),
+          CapabilityOptions.customCopilotRag(),
+          CapabilityOptions.customCopilotAssistant(),
+        ];
   }
 
   static tdpIntegrationCapabilities(): OptionItem[] {
@@ -774,6 +782,19 @@ export class CapabilityOptions {
         "core.createProjectQuestion.capability.customCopilotAssistantOption.detail"
       ),
       description: description,
+    };
+  }
+
+  // Currently only for dotnet
+  static customCopilotWeather(): OptionItem {
+    return {
+      id: "custom-copilot-weather-agent",
+      label: getLocalizedString(
+        "core.createProjectQuestion.capability.customCopilotWeatherOption.label"
+      ),
+      detail: getLocalizedString(
+        "core.createProjectQuestion.capability.customCopilotWeatherOption.detail"
+      ),
     };
   }
 }
