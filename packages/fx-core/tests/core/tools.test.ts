8e1cd7aa626e156f26f3a4830ebd4dc3fc183efd
Chenyi An <anchenyi@microsoft.com>
2025-04-10 12:16:04 +0800

    fix: ut

--- packages/fx-core/tests/core/tools.test.ts
+++ packages/fx-core/tests/core/tools.test.ts
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
+import { TokenCredential } from "@azure/identity";
+import { AzureAccountProvider, Settings, SubscriptionInfo } from "@microsoft/teamsfx-api";
 import { expect } from "chai";
 import "mocha";
 import { isValidProject, validateProjectSettings } from "../../src/common/projectSettingsHelper";
