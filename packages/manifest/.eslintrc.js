794d4dcbbc225d7cbc411df23cdd6ac9bd6d2af4
huajiezhang <huajiezhang@microsoft.com>
2025-04-15 12:46:37 +0800

    feat: include DA manifest schema files and types

--- packages/manifest/.eslintrc.js
+++ packages/manifest/.eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   extends: ["../eslint-plugin-teamsfx/config/shared.js"],
-  ignorePatterns: ["src/teams/MicrosoftTeams.**"], // Add folders to ignore here
+  ignorePatterns: [
+    "src/teams/MicrosoftTeams.**",
+    "src/copilot/declarative-agent/CopilotDeclarativeAgent.**",
+    "src/copilot/plugin/CopilotPlugin.**",
+  ], // Add folders to ignore here
   overrides: [
     {
       files: ["src/**/*.ts"],
