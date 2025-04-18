b4f63ff4c05809175bb062d1b245484cb7d96249
rentu <rentu>
2025-04-18 14:31:31 +0800

    fix: remove not ready code

--- packages/vscode-extension/src/handlers/sharedOpts.ts
+++ packages/vscode-extension/src/handlers/sharedOpts.ts
@@ -144,10 +144,6 @@ export async function runCommand(
         result = await core.addPlugin(inputs);
         break;
       }
-      case Stage.RegeneratePlugin: {
-        // result = await core.regeneratePlugin(inputs);
-        break;
-      }
       case Stage.kiotaRegenerate: {
         result = await core.kiotaRegenerate(inputs);
         break;
