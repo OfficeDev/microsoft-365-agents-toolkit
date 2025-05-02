5609cb8f677c841bd91627b87a1f804d1e1caab3
tmarwendo <tmarwendo+odspmdb@microsoft.com>
2025-05-02 10:53:54 -0700

    feat: update SPE Copilot template app to not hardcord constants

--- templates/ts/sharepoint-embedded-copilot/react-client/src/common/Constants.ts
+++ templates/ts/sharepoint-embedded-copilot/react-client/src/common/Constants.ts
@@ -1,13 +1,6 @@
-// export const REACT_APP_AZURE_SERVER_APP_ID = process.env.REACT_APP_AZURE_SERVER_APP_ID!;
-// export const AZURE_CLIENT_ID = process.env.REACT_APP_AZURE_APP_ID;
-// export const SPE_CONTAINER_TYPE_ID = process.env.REACT_APP_SPE_CONTAINER_TYPE_ID;
-// export const AUTH_AUTHORITY = `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`;
+export const REACT_APP_AZURE_SERVER_APP_ID = process.env.REACT_APP_AZURE_SERVER_APP_ID!;
+export const AZURE_CLIENT_ID = process.env.REACT_APP_AZURE_APP_ID;
+export const SPE_CONTAINER_TYPE_ID = process.env.REACT_APP_SPE_CONTAINER_TYPE_ID;
+export const AUTH_AUTHORITY = `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`;
 
-// export const SP_ROOT_SITE_URL = process.env.REACT_APP_SP_ROOT_SITE_URL;
-
-export const REACT_APP_AZURE_SERVER_APP_ID = "9ba5f667-0660-4603-a6eb-6aef199c7d46";
-export const AZURE_CLIENT_ID = "9ba5f667-0660-4603-a6eb-6aef199c7d46";
-export const SPE_CONTAINER_TYPE_ID = "b7b22e35-bd9e-499e-8cf8-974ce1ebeb32";
-export const AUTH_AUTHORITY = `https://login.microsoftonline.com/22527cfe-1fd7-4eca-b5b5-8c1e21ad3f62`;
-
-export const SP_ROOT_SITE_URL = "https://a830edad9050849takutest7.sharepoint.com";
+export const SP_ROOT_SITE_URL = process.env.REACT_APP_SP_ROOT_SITE_URL;
