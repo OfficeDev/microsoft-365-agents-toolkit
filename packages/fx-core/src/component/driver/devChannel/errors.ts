a6298351399676ed02e3028ada3f9ff55707d056
Ning Liu <nliu@microsoft.com>
2025-05-15 10:12:13 +0800

    fix: lint error

--- packages/fx-core/src/component/driver/devChannel/errors.ts
+++ packages/fx-core/src/component/driver/devChannel/errors.ts
@@ -8,10 +8,11 @@ export class InstallAppOutsideSandboxError extends UserError {
   constructor(actionName: string, helplink?: string) {
     super({
       source: actionName,
+      // eslint-disable-next-line no-secrets/no-secrets
       name: "InstallAppOutsideSandbox",
       message: getDefaultString("error.installApp.outsideSandbox"),
       displayMessage: getLocalizedString("error.installApp.outsideSandbox"),
-      helpLink: helplink
+      helpLink: helplink,
     });
   }
 }
