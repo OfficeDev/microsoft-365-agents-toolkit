eb3a2daa4863d97427ec52358e49df8e85ee2adc
wenyutang-ms <wenyutang@microsoft.com>
2025-05-30 10:39:44 +0800

    ci: update eslint rules

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ["../packages/eslint-plugin-teamsfx/config/shared.js"],
+  overrides: [
+    {
+      files: ["vsc/**/*.ts"],
+      rules: {
+        "no-redeclare": "off", // Allow redeclaration in VS Code extension files
+      },
+    },
+  ],
 };
