1c175ef9b080d81fc69f5862c4567034d75a9c03
Ning Liu <nliu@microsoft.com>
2025-07-14 14:53:24 +0800

    fix: update api report file

--- packages/sdk-react/src/index.ts
+++ packages/sdk-react/src/index.ts
@@ -1,27 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-/**
- * @deprecated This package will be deprecated by 2026-07.
- */
 export { useTeams } from "./useTeams";
-/**
- * @deprecated This package will be deprecated by 2026-07.
- */
 export { useData } from "./useData";
-/**
- * @deprecated This package will be deprecated by 2026-07.
- */
 export { useTeamsUserCredential, TeamsContextWithCredential } from "./useTeamsUserCredential";
-/**
- * @deprecated This package will be deprecated by 2026-07.
- */
 export { useGraphWithCredential } from "./useGraph";
-/**
- * @deprecated This package will be deprecated by 2026-07.
- */
 export { BaseDashboard } from "./BaseDashboard";
-/**
- * @deprecated This package will be deprecated by 2026-07.
- */
 export { BaseWidget, IWidgetClassNames } from "./BaseWidget";
