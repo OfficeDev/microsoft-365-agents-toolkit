63e742a89c49835eb6bfbb2942aff353e30ddfe7
Ning Tang <nintan@microsoft.com>
2025-07-30 15:58:22 +0800

    fix: code update

--- packages/fx-core/src/question/inputs/ShareInputs.ts
+++ packages/fx-core/src/question/inputs/ShareInputs.ts
@@ -15,6 +15,6 @@ export interface ShareInputs extends Inputs {
   "share-operation"?: "share" | "unshare";
   /** @description Share the agent with users */
   scope?: "tenant" | "users" | "owners";
-  /** @description Email addresses of user to share the agent with */
+  /** @description Email addresses of users for agent sharing */
   email?: string;
 }
