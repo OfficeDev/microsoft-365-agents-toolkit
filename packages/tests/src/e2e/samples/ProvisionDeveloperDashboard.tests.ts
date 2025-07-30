0f8abd88dc2ed76615eabf221e8839d6bd02b04c
Bowen Song <bowsong@microsoft.com>
2025-07-30 13:22:10 +0800

    test: fix e2e test for developer dashboard

--- packages/tests/src/e2e/samples/ProvisionDeveloperDashboard.tests.ts
+++ packages/tests/src/e2e/samples/ProvisionDeveloperDashboard.tests.ts
@@ -16,11 +16,12 @@ class AssistDashboardTestCase extends CaseFactory {
   public override async onAfterCreate(projectPath: string): Promise<void> {
     expect(fs.pathExistsSync(path.resolve(projectPath, "infra"))).to.be.true;
     // remove teamsApp/extendToM365 in case it fails
-    removeTeamsAppExtendToM365(path.join(projectPath, "teamsapp.yml"));
+    removeTeamsAppExtendToM365(path.join(projectPath, "m365agents.yml"));
 
     const envFilePath = path.resolve(projectPath, "env", `.env.dev.user`);
-    const envString =
-      'PLANNER_GROUP_ID=YOUR_PLANNER_GROUP_ID\nDEVOPS_ORGANIZATION_NAME=msazure\nDEVOPS_PROJECT_NAME="Microsoft Teams Extensibility"\nGITHUB_REPO_NAME=test002\nGITHUB_REPO_OWNER=hellyzh\nPLANNER_PLAN_ID=YOUR_PLAN_ID\nPLANNER_BUCKET_ID=YOUR_BUCKET_ID\nSECRET_DEVOPS_ACCESS_TOKEN=YOUR_DEVOPS_ACCESS_TOKEN\nSECRET_GITHUB_ACCESS_TOKEN=YOUR_GITHUB_ACCESS_TOKEN';
+    let envString = fs.readFileSync(envFilePath, "utf-8");
+    envString +=
+      '\nPLANNER_GROUP_ID=YOUR_PLANNER_GROUP_ID\nDEVOPS_ORGANIZATION_NAME=msazure\nDEVOPS_PROJECT_NAME="Microsoft Teams Extensibility"\nGITHUB_REPO_NAME=test002\nGITHUB_REPO_OWNER=hellyzh\nPLANNER_PLAN_ID=YOUR_PLAN_ID\nPLANNER_BUCKET_ID=YOUR_BUCKET_ID\nSECRET_DEVOPS_ACCESS_TOKEN=YOUR_DEVOPS_ACCESS_TOKEN\nSECRET_GITHUB_ACCESS_TOKEN=YOUR_GITHUB_ACCESS_TOKEN';
     fs.writeFileSync(envFilePath, envString);
   }
 }
