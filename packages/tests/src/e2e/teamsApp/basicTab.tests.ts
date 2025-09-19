25883853e79b12328e48ea5ff4e0c12049372814
Ning Tang <nintan@microsoft.com>
2025-09-19 11:07:20 +0800

    feat(e2e): add basic tab test case

--- packages/tests/src/e2e/teamsApp/basicTab.tests.ts
+++ packages/tests/src/e2e/teamsApp/basicTab.tests.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 /**
- * @author Zhijie Huang <zhihuan@microsoft.com>
+ * @author Zhijie Huang <zhihuan@microsoft.com>, Ning Tang
  */
 
 import { it } from "@microsoft/extra-shot-mocha";
@@ -28,6 +28,7 @@ import {
   readContextMultiEnvV3,
   removeTeamsAppExtendToM365,
 } from "../commonUtils";
+import { getTeamsApp } from "../debug/utility";
 
 describe("Basic Tab", function () {
   const testFolder = getTestFolder();
@@ -36,96 +37,146 @@ describe("Basic Tab", function () {
   const resourceGroupName = `${appName}-rg`;
   const projectPath = path.resolve(testFolder, appName);
   const envName = environmentNameManager.getDefaultEnvName();
+
   afterEach(async () => {
     await cleanUp(appName, projectPath, false, false, false);
   });
 
-  const files = ["index.ts"];
-  const languages = ["typescript"];
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    const language = languages[i];
-    it(
-      `Create & Provision & Deploy Basic Tab (${language})`,
-      {
-        testPlanCaseId: language === "javascript" ? 9426074 : 24137515,
-        author: "Ning.Tang@microsoft.com",
-      },
-      async function () {
-        // Scaffold
-        await CliHelper.createProjectWithCapability(
-          appName,
-          testFolder,
-          Capability.TabNonSso,
-          process.env,
-          `--programming-language ${language}`
-        );
+  it(
+    "typescript template",
+    {
+      testPlanCaseId: 24137515,
+      author: "Ning.Tang@microsoft.com",
+    },
+    async function () {
+      // Scaffold
+      await CliHelper.createProjectWithCapability(
+        appName,
+        testFolder,
+        Capability.TabNonSso,
+        process.env,
+        `--programming-language typescript`
+      );
 
-        // Validate Scaffold
-        const indexFile = path.join(projectPath, "src", file);
-        fs.access(indexFile, fs.constants.F_OK, (err) => {
-          assert.notExists(err);
-        });
+      // Validate Scaffold
+      const indexFile = path.join(projectPath, "src", "index.ts");
+      fs.access(indexFile, fs.constants.F_OK, (err) => {
+        assert.notExists(err, "index.ts should exist");
+      });
 
-        // remove teamsApp/extendToM365 in case it fails
-        removeTeamsAppExtendToM365(path.join(projectPath, "m365agents.yml"));
+      // remove teamsApp/extendToM365 in case it fails
+      removeTeamsAppExtendToM365(path.join(projectPath, "m365agents.yml"));
+      removeTeamsAppExtendToM365(
+        path.join(projectPath, "m365agents.local.yml")
+      );
 
-        // Provision
-        const result = await createResourceGroup(resourceGroupName, "westus");
-        assert.isTrue(result);
+      // Local Debug (Provision)
+      await CliHelper.provisionProject(projectPath, "", "local");
+      console.log(`[Successfully] provision for ${projectPath}`);
 
-        // await setProvisionParameterValueV3(projectPath, envName, {
-        //   key: "webAppSku",
-        //   value: "B1",
-        // });
-        await CliHelper.provisionProject(projectPath, "", envName as "dev", {
-          ...process.env,
-          AZURE_RESOURCE_GROUP_NAME: resourceGroupName,
-        });
+      let context = await readContextMultiEnvV3(projectPath, "local");
+      chai.assert.isDefined(context, "local env file should exist");
 
-        // Validate Provision
-        let context = await readContextMultiEnvV3(projectPath, envName);
-        assert.exists(context);
+      // validate aad
+      chai.assert.isUndefined(
+        context.AAD_APP_OBJECT_ID,
+        "AAD should not exist"
+      );
 
-        const resourceId =
-          context[EnvConstants.TAB_AZURE_APP_SERVICE_RESOURCE_ID];
-        assert.exists(resourceId);
+      // validate teams app
+      chai.assert.isDefined(
+        context.TEAMS_APP_ID,
+        "teams app id should be defined"
+      );
+      const teamsApp = await getTeamsApp(context.TEAMS_APP_ID);
+      chai.assert.equal(teamsApp?.teamsAppId, context.TEAMS_APP_ID);
 
-        const tokenProvider = MockAzureAccountProvider;
-        const tokenCredential =
-          await tokenProvider.getIdentityCredentialAsync();
-        const token = (await tokenCredential?.getToken(AzureScopes))?.token;
-        assert.exists(token);
+      // Local Debug (Deploy)
+      await CliHelper.deployAll(projectPath, "", "local");
+      console.log(`[Successfully] deploy for ${projectPath}`);
 
-        const response = await getWebappSettings(
-          subscription,
-          getResourceGroupNameFromResourceId(resourceId),
-          getSiteNameFromResourceId(resourceId),
-          token as string
-        );
-        assert.exists(response);
-        assert.equal(response["WEBSITE_NODE_DEFAULT_VERSION"], "~22");
-        assert.equal(response["WEBSITE_RUN_FROM_PACKAGE"], "1");
-        assert.equal(response["RUNNING_ON_AZURE"], "1");
+      context = await readContextMultiEnvV3(projectPath, "local");
+      chai.assert.isDefined(context);
 
-        // deploy
-        await CliHelper.deployAll(projectPath);
+      // validate ssl cert
+      chai.assert.isDefined(context.SSL_CRT_FILE, "ssl cert should be defined");
+      chai.assert.isNotEmpty(context.SSL_CRT_FILE);
+      chai.assert.isDefined(context.SSL_KEY_FILE, "ssl key should be defined");
+      chai.assert.isNotEmpty(context.SSL_KEY_FILE);
 
-        // Validate Deploy
-        context = await readContextMultiEnvV3(projectPath, envName);
-        assert.exists(context);
+      // validate .localConfigs
+      chai.assert.isTrue(
+        await fs.pathExists(path.join(projectPath, ".localConfigs")),
+        ".localConfigs should exist"
+      );
 
-        const endpoint = context[EnvConstants.TAB_ENDPOINT];
-        assert.exists(endpoint);
+      // Remote Provision
+      const result = await createResourceGroup(resourceGroupName, "westus");
+      assert.isTrue(
+        result,
+        `failed to create resource group: ${resourceGroupName}`
+      );
 
-        const axiosInstance = axios.create();
-        try {
-          const response = await axiosInstance.get(endpoint);
-          assert.equal(response.status, 200);
-        } catch (e) {
-          assert.fail(JSON.stringify(e));
-        }
+      await CliHelper.provisionProject(projectPath, "", "dev", {
+        ...process.env,
+        AZURE_RESOURCE_GROUP_NAME: resourceGroupName,
+      });
+
+      context = await readContextMultiEnvV3(projectPath, envName);
+      assert.exists(context, "env file should exist");
+
+      const appServiceResourceId =
+        context[EnvConstants.TAB_AZURE_APP_SERVICE_RESOURCE_ID];
+      assert.exists(
+        appServiceResourceId,
+        "Azure App Service resource ID should exist"
+      );
+
+      const tokenProvider = MockAzureAccountProvider;
+      const tokenCredential = await tokenProvider.getIdentityCredentialAsync();
+      const token = (await tokenCredential?.getToken(AzureScopes))?.token;
+      assert.exists(token);
+
+      const response = await getWebappSettings(
+        subscription,
+        getResourceGroupNameFromResourceId(appServiceResourceId),
+        getSiteNameFromResourceId(appServiceResourceId),
+        token as string
+      );
+      assert.exists(response, "Web app settings should exist");
+      assert.equal(
+        response["WEBSITE_NODE_DEFAULT_VERSION"],
+        "~22",
+        "Node version should be 22"
+      );
+      assert.equal(
+        response["WEBSITE_RUN_FROM_PACKAGE"],
+        "1",
+        "Run from package should be 1"
+      );
+      assert.equal(
+        response["RUNNING_ON_AZURE"],
+        "1",
+        "Running on azure should be 1"
+      );
+
+      // Remote Deploy
+      await CliHelper.deployAll(projectPath);
+
+      // Validate Deploy
+      context = await readContextMultiEnvV3(projectPath, envName);
+      assert.exists(context, "env file should exist");
+
+      const endpoint = context[EnvConstants.TAB_ENDPOINT];
+      assert.exists(endpoint, "Tab endpoint should exist");
+
+      const axiosInstance = axios.create();
+      try {
+        const response = await axiosInstance.get(endpoint);
+        assert.equal(response.status, 200, "tab endpoint is not reachable");
+      } catch (e) {
+        assert.fail(JSON.stringify(e));
       }
-    );
-  }
+    }
+  );
 });
