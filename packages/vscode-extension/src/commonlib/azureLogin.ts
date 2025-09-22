7bdc7eae04015bd8f0ffebf617fcfe664b86a312
Huihui Wu <huihuiwu@microsoft.com>
2025-09-22 12:51:10 +0800

    fix: get clamis not found in challenge error

--- packages/vscode-extension/src/commonlib/azureLogin.ts
+++ packages/vscode-extension/src/commonlib/azureLogin.ts
@@ -181,10 +181,9 @@ export class AzureAccountManager extends login implements AzureAccountProvider {
   }
 
   private async doGetIdentityCredentialAsync(
-    showDialog?: boolean,
-    wwwAuthenticate?: string
+    showDialog?: boolean
   ): Promise<TokenCredential | undefined> {
-    const tokenCredential = await this.doGetAccountCredentialAsync(wwwAuthenticate);
+    const tokenCredential = await this.doGetAccountCredentialAsync();
     if (tokenCredential) {
       return tokenCredential;
     } else {
@@ -192,9 +191,7 @@ export class AzureAccountManager extends login implements AzureAccountProvider {
     }
   }
 
-  private async doGetAccountCredentialAsync(
-    wwwAuthenticate?: string
-  ): Promise<TokenCredential | undefined> {
+  private async doGetAccountCredentialAsync(): Promise<TokenCredential | undefined> {
     if (await this.isUserLogin()) {
       const subs = await this.vscodeAzureSubscriptionProvider.getSubscriptions();
       if (subs.length > 0) {
@@ -208,14 +205,10 @@ export class AzureAccountManager extends login implements AzureAccountProvider {
         }
         return subs[0].credential;
       } else {
-        const session = await getSessionFromVSCode(
-          { scopes: AzureScopes, wwwAuthenticate: wwwAuthenticate ?? "" },
-          undefined,
-          {
-            createIfNone: false,
-            silent: true,
-          }
-        );
+        const session = await getSessionFromVSCode(AzureScopes, undefined, {
+          createIfNone: false,
+          silent: true,
+        });
         const credential: TokenCredential = {
           // eslint-disable-next-line @typescript-eslint/require-await
           getToken: async () => {
