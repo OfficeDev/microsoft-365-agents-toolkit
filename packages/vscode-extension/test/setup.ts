d2a6b0fb00d2f957003fca6cf1aea402c2c91b5f
Huihui Wu <huihuiwu@microsoft.com>
2025-12-18 13:00:51 +0800

    test: fix UT mock issue

--- packages/vscode-extension/test/setup.ts
+++ packages/vscode-extension/test/setup.ts
@@ -2,25 +2,6 @@
 // Licensed under the MIT License.
 "use strict";
 
-// Mock keytar to prevent native module loading issues on Linux
-// keytar is used by @azure/msal-node-extensions for secure credential storage,
-// but is not needed in unit tests where we mock authentication
-const Module = require("module");
-const originalRequire = Module.prototype.require;
-
-Module.prototype.require = function (id: string, ...args: any[]) {
-  if (id === "keytar") {
-    return {
-      getPassword: async () => null,
-      setPassword: async () => {},
-      deletePassword: async () => true,
-      findPassword: async () => null,
-      findCredentials: async () => [],
-    };
-  }
-  return originalRequire.apply(this, [id, ...args]);
-};
-
 import * as child_process from "child_process";
 import * as os from "os";
 if (os.platform() === "win32") {
