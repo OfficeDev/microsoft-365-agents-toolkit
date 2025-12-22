32a3311ced8a2d5196a2b81dd11739b4c3ada474
Zhiyu You <zhiyou@microsoft.com>
2025-12-22 14:13:34 +0800

    Revert "fix: mark mcp-tools.json as read-only"
    This reverts commit 1054bd622221e09a09fb76b41c3af74559dcf706.

--- packages/fx-core/src/core/FxCore.ts
+++ packages/fx-core/src/core/FxCore.ts
@@ -3136,9 +3136,6 @@ export class FxCore {
           suffix += 1;
         }
       }
-      if (await fs.pathExists(path.join(path.dirname(aiPluginFilePath), mcpFile))) {
-        await fs.chmod(path.join(path.dirname(aiPluginFilePath), mcpFile), 0o600); // remove read-only
-      }
       await fs.writeJSON(
         path.join(path.dirname(aiPluginFilePath), mcpFile),
         {
@@ -3155,7 +3152,7 @@ export class FxCore {
               }),
           ],
         },
-        { spaces: 4, mode: 0o400 }
+        { spaces: 4 }
       );
       (aiPluginContent.runtimes as any[]).push({
         type: "RemoteMCPServer",
