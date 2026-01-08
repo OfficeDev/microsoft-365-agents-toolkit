a84db9ff64d4495808dc80711572cbccae022ddc
Huihui Wu <huihuiwu@microsoft.com>
2026-01-08 10:13:03 +0800

    fix: revert install libsecret in ci pipeline and dynamic load @azure/msal-node-extensions

--- packages/cli/src/commonlib/m365Login.ts
+++ packages/cli/src/commonlib/m365Login.ts
@@ -4,7 +4,6 @@
 "use strict";
 
 import { Configuration, LogLevel } from "@azure/msal-node";
-import { NativeBrokerPlugin } from "@azure/msal-node-extensions";
 import {
   BasicLogin,
   err,
@@ -32,32 +31,43 @@ const SERVER_PORT = 0;
 
 const cachePlugin = new CryptoCachePlugin(m365CacheName);
 
-const config: Configuration = {
-  auth: {
-    clientId: "7ea7c24c-b1f6-4a20-9d11-9ae12e9e7ac0",
-    authority: "https://login.microsoftonline.com/common",
-  },
-  broker: {
-    nativeBrokerPlugin:
-      featureFlagManager.getBooleanValue(FeatureFlags.BrokerAuth) && process.platform === "win32"
-        ? new NativeBrokerPlugin()
-        : undefined,
-  },
-  system: {
-    loggerOptions: {
-      loggerCallback(loglevel: any, message: any, containsPii: any) {
-        if (this.logLevel && this.logLevel <= LogLevel.Error) {
-          CLILogProvider.log(4 - loglevel, message);
-        }
-      },
-      piiLoggingEnabled: false,
-      logLevel: LogLevel.Error,
+function getConfig(): Configuration {
+  const config: Configuration = {
+    auth: {
+      clientId: "7ea7c24c-b1f6-4a20-9d11-9ae12e9e7ac0",
+      authority: "https://login.microsoftonline.com/common",
     },
-  },
-  cache: {
-    cachePlugin,
-  },
-};
+    system: {
+      loggerOptions: {
+        loggerCallback(loglevel: any, message: any, containsPii: any) {
+          if (this.logLevel && this.logLevel <= LogLevel.Error) {
+            CLILogProvider.log(4 - loglevel, message);
+          }
+        },
+        piiLoggingEnabled: false,
+        logLevel: LogLevel.Error,
+      },
+    },
+    cache: {
+      cachePlugin,
+    },
+  };
+
+  if (featureFlagManager.getBooleanValue(FeatureFlags.BrokerAuth) && process.platform === "win32") {
+    try {
+      // Dynamically require to avoid loading @azure/msal-node-extensions on Linux
+      // where keytar (a dependency) requires libsecret to be installed
+      const { NativeBrokerPlugin } = require("@azure/msal-node-extensions");
+      config.broker = {
+        nativeBrokerPlugin: new NativeBrokerPlugin(),
+      };
+    } catch {
+      // NativeBrokerPlugin not available, ignore
+    }
+  }
+
+  return config;
+}
 
 export class M365Login extends BasicLogin implements M365TokenProvider {
   private static instance: M365Login;
@@ -65,7 +75,7 @@ export class M365Login extends BasicLogin implements M365TokenProvider {
 
   private constructor() {
     super();
-    M365Login.codeFlowInstance = new CodeFlowLogin([], config, SERVER_PORT, m365CacheName);
+    M365Login.codeFlowInstance = new CodeFlowLogin([], getConfig(), SERVER_PORT, m365CacheName);
   }
 
   /**
