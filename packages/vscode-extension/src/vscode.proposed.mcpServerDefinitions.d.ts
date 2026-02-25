eb417a56d52cdca0fd0acb9c15451b954ba9fcd9
Zhiyu You <zhiyou@microsoft.com>
2026-02-25 09:35:52 +0800

    fix: lint

--- packages/vscode-extension/src/vscode.proposed.mcpServerDefinitions.d.ts
+++ packages/vscode-extension/src/vscode.proposed.mcpServerDefinitions.d.ts
@@ -1,9 +1,8 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
-declare module 'vscode' {
+
+declare module "vscode" {
 
 	// https://github.com/microsoft/vscode/issues/288777 @DonJayamanne
 
