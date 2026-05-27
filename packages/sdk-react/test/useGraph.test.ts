1a90362caeb202f52cfc5ae8c2973ce2318fc896
Neil Ye (Centific Technologies Inc) <v-cheye@microsoft.com>
2026-05-27 17:08:12 +0800

    fix: remove invalid link https://aka.ms/teamsfx-auth-code-flow

--- packages/sdk-react/test/useGraph.test.ts
+++ packages/sdk-react/test/useGraph.test.ts
@@ -224,7 +224,7 @@ describe("useGraphWithCredential() hook tests", () => {
           "CancelledByUser" +
             '\nIf you see "AADSTS50011: The reply URL specified in the request does not match the reply URLs configured for the application" ' +
             "in the popup window, you may be using unmatched version for TeamsFx SDK (version >= 0.5.0) and Teams Toolkit (version < 3.3.0) or " +
-            `cli (version < 0.11.0). Please refer to the help link for how to fix the issue: https://aka.ms/teamsfx-auth-code-flow`,
+            "cli (version < 0.11.0).",
         );
       },
       { interval: 1 },
