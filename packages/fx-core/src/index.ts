65c1154bff7a3ad737e71da9a660bfabad9d014d
Zhiyu You <zhiyou@microsoft.com>
2026-06-25 15:53:01 +0800

    fix(mcp): improve remote auth discovery

--- packages/fx-core/src/index.ts
+++ packages/fx-core/src/index.ts
@@ -109,6 +109,7 @@ export * from "./component/middleware/actionExecutionMW";
 export { outputScaffoldingWarningMessage } from "./component/utils/common";
 export { DotenvOutput, envUtil } from "./component/utils/envUtil";
 export { metadataUtil } from "./component/utils/metadataUtil";
+export { MCPAuthProbeResult, probeMCPServerAuth } from "./component/utils/mcpToolFetcher";
 export { ODRTool, ODRServer, ODRProvider } from "./component/utils/odrProvider";
 export { pathUtils } from "./component/utils/pathUtils";
 export { newResourceGroupOption, resourceGroupHelper } from "./component/utils/ResourceGroupHelper";
--- templates/vsc/common/declarative-agent-with-action-from-mcp/m365agents.yml.tpl
+++ templates/vsc/common/declarative-agent-with-action-from-mcp/m365agents.yml.tpl
@@ -1,7 +1,7 @@
-# yaml-language-server: $schema=https://aka.ms/m365-agents-toolkits/v1.12/yaml.schema.json
+# yaml-language-server: $schema=https://aka.ms/m365-agents-toolkits/v1.13/yaml.schema.json
 # Visit https://aka.ms/teamsfx-v5.0-guide for details on this file
 # Visit https://aka.ms/teamsfx-actions for details on actions
-version: v1.12
+version: v1.13
 
 environmentFolderPath: ./env
 
